8327cf52eab3d3d01e27364fd32f218de669d9f5
tonio <anntonio@protonmail.ch>
2020-02-08 22:47:02 -0200

    added basic movement, bad gravity and hand holster

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public GameObject hand;
+    public Transform player;
+    public float mouseSensitivity;
+    public float holsterTimeThreshold = 0.5f;
+
+    float holdTime = 0.0f;
+
+    void Update()
+    {
+        checkHolster();
+        look();
+    }
+
+    void look()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        
+        Vector3 yRotation = -1 * Vector3.right * mouseY;
+        transform.Rotate(yRotation * mouseSensitivity * Time.deltaTime);
+        Debug.Log(transform.localEulerAngles);
+        
+        Vector3 xRotation = Vector3.up * mouseX;
+        player.Rotate(xRotation * mouseSensitivity * Time.deltaTime);
+    }
+
+    void checkHolster()
+    {
+        KeyCode key = KeyCode.R;
+        
+        if (Input.GetKeyDown(key) && hand.activeSelf == false)
+            hand.SetActive(!(hand.activeSelf));
+        
+        if (holdTime >= holsterTimeThreshold && hand.activeSelf == true)
+            hand.SetActive(!(hand.activeSelf));
+
+        else if (Input.GetKey(key))
+            holdTime += Time.deltaTime;
+        
+        else if (Input.GetKeyUp(key))
+            holdTime = 0.0f; 
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float gravity = 9.8f;
+
+    public CharacterController characterController;
+    
+    Vector3 movement;
+    Vector3 gravMovement;
+
+    void Update()
+    { 
+        move();
+        applyGravity();
+    }
+
+    void move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        movement = transform.right * x + transform.forward * z;
+        characterController.Move(movement * speed * Time.deltaTime);
+    }
+
+    void applyGravity()
+    {
+        gravMovement += Vector3.down * gravity * (Time.deltaTime * Time.deltaTime);
+        characterController.Move(gravMovement);
+    }
+}
+
