e4a9ac67908f50c59f8f90b263b3dd78ab5fa253
tonio <anntonio@protonmail.ch>
2021-01-25 10:57:58 -0300

    Brand new beginning
    fp movement already implemented

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerLook : MonoBehaviour
+{
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    public float mouseSensitivity = 100f;
+
+    int cameraCounter = 0;
+    public Camera[] alternativeCameras;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.C)) {
+            alternativeCameras[0].gameObject.SetActive(true);
+        }
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
